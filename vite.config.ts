594bf49a69d9a56d0a2410e911c1359c5a9347c5
Arshia Kamali <arshiakamali493@gmail.com>
2026-09-21 11:24:46 +0330

    Configure GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/frontend-admin/",
   server: { port: 5174 },
   build: { outDir: "dist", sourcemap: false },
 });
